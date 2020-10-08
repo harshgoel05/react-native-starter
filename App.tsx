6a0de87b876ed60c5511058799e8deb173b51d7a
harshgoel05 <harshgoel05@gmail.com>
2020-10-08 12:55:04 +0530

    initalize

--- App.tsx
+++ App.tsx
@@ -1,21 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { Component } from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  state = {
+    name: "hi",
+  };
+  handleChange = (event: any) => {
+    console.log(event);
+    this.setState({
+      name: event,
+    });
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello World!</Text>
+        <TextInput
+          style={styles.inp}
+          value={this.state.name}
+          onChangeText={this.handleChange}
+        ></TextInput>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inp: {
+    borderWidth: 1,
+    borderColor: "green",
+    minWidth: 100,
   },
 });
